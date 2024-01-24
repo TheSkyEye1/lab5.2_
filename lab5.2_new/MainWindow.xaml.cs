72a057efbb6fb4a9a67098bf8b828af4b58473d0
TheSkyEye1 <golovan4ickoff@gmail.com>
2024-01-24 18:47:48 +0700

    save

--- lab5.2_new/MainWindow.xaml.cs
+++ lab5.2_new/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml.Linq;
 
 namespace lab5._2_new
 {
@@ -20,9 +22,256 @@ namespace lab5._2_new
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<tree> Trees = new List<tree>();
+        int startPop = 10;
+        Random rnd = new Random();
+        int radius = 30;
+        int maxMutChanse = 85;
+        int minMutChanse = 25;
+        int maxLifeTime = 5;
+        int minLifeTime = 2;
+        int radiusCoef = 2;
+        int treesInRadiusMin = 3;
+        int iteration = 0;
+        int unchangedIteration = 0;
+        int treeCount = 0;
+        DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += Timer_Tick;
+        }
+
+        public void CreatePopulation()
+        {
+            for (int i = 0; i < startPop; i++)
+            {
+                double x = radius + (scene.Width - radius) * rnd.NextDouble();
+                double y = radius + (scene.Height - radius) * rnd.NextDouble();
+
+                List<tree> tr = new List<tree>();
+                tr.AddRange(Trees);
+                Trees.Add(new tree(x, y, rnd, rnd.Next(minMutChanse, maxMutChanse), rnd.Next(minLifeTime, maxLifeTime), radius, scene.Height, scene.Width));
+                Trees[i].age = 0;
+                foreach (tree t in tr)
+                {
+                    if (Trees[i].IsIntersecting(t))
+                    {
+                        Trees.Remove(Trees[i]);
+                        i--;
+                        break;
+                    }
+                }
+
+            }
+
+            treeCount = Trees.Count;
+        }
+
+        public void CalcilateFitness()
+        {
+            foreach (tree t in Trees)
+            {
+                t.fitness = int.MaxValue;
+                t.TreesInRadius = new List<tree>();
+            }
+
+            for (int i = 0; i < Trees.Count; i++)
+            {
+                List<tree> treesinradius = new List<tree>();
+
+                for (int j = 0; j < Trees.Count; j++)
+                {
+                    if (j != i && !Trees[i].TreesInRadius.Contains(Trees[j]))
+                    {
+                        double dist = Trees[i].DistanceTo(Trees[j].x, Trees[j].y);
+                        if (dist <= radius * radiusCoef)
+                        {
+                            Trees[i].fitness += dist;
+                            Trees[j].fitness += dist;
+                            Trees[i].TreesInRadius.Add(Trees[j]);
+                            Trees[j].TreesInRadius.Add(Trees[i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void NextGeneration()
+        {
+            List<tree> parents = new List<tree>();
+
+            for (int i = 0; i < Trees.Count / 2 + 2; i++)
+            {
+                int id = rnd.Next(Trees.Count);
+
+                if (parents.Contains(Trees[id])) i--;
+                else parents.Add(Trees[id]);
+            }
+
+            parents.Sort((x, y) => x.fitness.CompareTo(y.fitness));
+
+            while (parents.Count > 1)
+            {
+                int index1 = rnd.Next(parents.Count);
+                int index2 = index1;
+                while (index1 == index2)
+                {
+                    index2 = rnd.Next(parents.Count);
+                }
+                tree parent1 = parents[index1];
+                tree parent2 = parents[index2];
+
+                tree children = parent1.crossover(parent2);
+                children.TreesInRadius = new List<tree>();
+
+                bool intersect = false;
+
+                foreach (tree t in Trees)
+                {
+                    if (children.IsIntersecting(t))
+                    {
+                        intersect = true;
+                        break;
+                    }
+                }
+
+                if (!intersect) Trees.Add(children);
+
+                parents.Remove(parent2);
+                parents.Remove(parent1);
+            }
+
+            for (int i = 0; i < Trees.Count; i++)
+            {
+                Trees[i].mutate();
+
+                if (Trees[i].TreesInRadius.Count < treesInRadiusMin)
+                {
+                    Trees[i].age += 1;
+                }
+
+                if (Trees[i].age == Trees[i].lifeTime)
+                {
+                    Trees.Remove(Trees[i]);
+                }
+            }
+
+            if (Trees.Count < startPop)
+            {
+                for (int i = Trees.Count; i < startPop + 1; i++)
+                {
+                    double x = radius + (scene.Width - radius) * rnd.NextDouble();
+                    double y = radius + (scene.Height - radius) * rnd.NextDouble();
+
+                    List<tree> tr = new List<tree>();
+                    tr.AddRange(Trees);
+                    Trees.Add(new tree(x, y, rnd, rnd.Next(minMutChanse, maxMutChanse), rnd.Next(minLifeTime, maxLifeTime), radius, scene.Height, scene.Width));
+                    Trees[i].age = 0;
+                    foreach (tree t in tr)
+                    {
+                        if (Trees[i].IsIntersecting(t))
+                        {
+                            Trees.Remove(Trees[i]);
+                            i--;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public void DrawCircle(double x, double y)
+        {
+            Ellipse el = new Ellipse();
+            SolidColorBrush cb = new SolidColorBrush();
+            cb.Color = Color.FromArgb(255, 0, 255, 0);
+            el.Fill = cb;
+            el.StrokeThickness = 0;
+            el.Stroke = Brushes.Black;
+            el.Width = radius;
+            el.Height = radius;
+            el.RenderTransform = new TranslateTransform(x - radius / 2, y - radius / 2);
+            scene.Children.Add(el);
+        }
+
+        public void DrawScene()
+        {
+            scene.Children.Clear();
+            foreach (tree t in Trees)
+            {
+                DrawCircle(t.x, t.y);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Trees.Count == 0)
+            {
+                Trees = new List<tree>();
+                CreatePopulation();
+                DrawScene();
+                timer.Start();
+            }
+            else
+            {
+                if (timer.IsEnabled) timer.Stop();
+                else timer.Start();
+            }
+        }
+
+        public void AddNewPop()
+        {
+            for (int i = Trees.Count; i < 5; i++)
+            {
+                double x = radius + (scene.Width - radius) * rnd.NextDouble();
+                double y = radius + (scene.Height - radius) * rnd.NextDouble();
+
+                List<tree> tr = new List<tree>();
+                tr.AddRange(Trees);
+                Trees.Add(new tree(x, y, rnd, rnd.Next(minMutChanse, maxMutChanse), rnd.Next(minLifeTime, maxLifeTime), radius, scene.Height, scene.Width));
+                Trees[i].age = 0;
+                foreach (tree t in tr)
+                {
+                    if (Trees[i].IsIntersecting(t))
+                    {
+                        Trees.Remove(Trees[i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            iteration++;
+
+            CalcilateFitness();
+            NextGeneration();
+
+            if(treeCount == Trees.Count)
+            {
+                unchangedIteration++;
+                if(unchangedIteration == 15)
+                {
+                    AddNewPop();
+                }
+            }
+            else
+            {
+                treeCount = Trees.Count;
+                unchangedIteration = 0;
+            }
+
+            DrawScene();
+
+            treesLB.Content = "Trees: " + Trees.Count;
+            iterLB.Content = "Iteration: " + iteration;
+            unchitLB.Content = "Unchanged Iter: " + unchangedIteration;
         }
     }
 }
--- lab5.2_new/tree.cs
+++ lab5.2_new/tree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace lab5._2_new
 {
@@ -10,44 +11,118 @@ namespace lab5._2_new
     {
         public double x;
         public double y;
-        public double amount;
         Random rnd;
-        public tree(double x, double y, Random rnd) 
+        public List<tree> TreesInRadius;
+        public int mutationChanse;
+        public int lifeTime;
+        public int age;
+        public double fitness = int.MaxValue;
+        public int radius;
+        double H;
+        double W;
+
+        public tree(double x, double y, Random rnd, int mutationChanse, int lifeTime, int radius, double H, double W) 
         {
             this.x = x;
             this.y = y;
             this.rnd = rnd;
+            this.mutationChanse = mutationChanse;
+            this.lifeTime = lifeTime;
+            this.radius = radius;
+            this.H = H;
+            this.W = W;
         }
-
         public void mutate()
         {
-            if(rnd.Next(4) > 0)
+            bool intersecting = false;
+            foreach (tree tree in TreesInRadius)
             {
-                if(rnd.Next(2) == 0)
+                if (IsIntersecting(tree))
                 {
-                    if(rnd.Next(2) == 0)
-                    {
-                        x += 1 + (5 - 1) * rnd.NextDouble();
-                    }
-                    else
-                    {
-                        x += 1 + (5 - 1) * rnd.NextDouble();
-                    }
+                    intersecting = true;
+                    break;
                 }
-                else
+            }
+            if (!intersecting)
+            {
+                if (rnd.Next(100) < mutationChanse)
                 {
                     if (rnd.Next(2) == 0)
                     {
-                        y += 1 + (5 - 1) * rnd.NextDouble();
+                        if (rnd.Next(2) == 0)
+                        {
+                            double coef = 1 + (5 - 1) * rnd.NextDouble();
+                            if (x + coef + radius > W) x = W - radius;
+                            else x += 1 + (5 - 1) * rnd.NextDouble();
+                        }
+                        else
+                        {
+                            double coef = 1 + (5 - 1) * rnd.NextDouble();
+                            if (x - coef < radius) x = radius;
+                            else x -= 1 + (5 - 1) * rnd.NextDouble();
+                        }
                     }
                     else
                     {
-                        y += 1 + (5 - 1) * rnd.NextDouble();
+                        if (rnd.Next(2) == 0)
+                        {
+                            double coef = 1 + (5 - 1) * rnd.NextDouble();
+                            if (y + coef + radius > H) y = H - radius;
+                            else y += 1 + (5 - 1) * rnd.NextDouble();
+                        }
+                        else
+                        {
+                            double coef = 1 + (5 - 1) * rnd.NextDouble();
+                            if (y - coef < radius) x = radius;
+                            else y -= 1 + (5 - 1) * rnd.NextDouble();
+                        }
                     }
                 }
             }
         }
 
+        public tree crossover(tree parent2)
+        {
+            double newX;
+            double newY;
+
+            if (rnd.Next(2) == 0) newX = x;
+            else newX = parent2.x;
+            if (rnd.Next(2) == 0) newY = y;
+            else newY = parent2.y;
+
+            int newlifetime;
+
+            if (lifeTime > parent2.lifeTime) newlifetime = rnd.Next(parent2.lifeTime, lifeTime + 1);
+            else if (lifeTime < parent2.lifeTime) newlifetime = rnd.Next(lifeTime, parent2.lifeTime + 1);
+            else newlifetime = rnd.Next(lifeTime + 1);
+
+            int mut;
+            if (rnd.Next(2) == 0) mut = mutationChanse;
+            else mut = parent2.mutationChanse;
+
+            return new tree(newX, newY, rnd, mut, newlifetime, radius, H, W);
+        }
+
+        public bool IsIntersecting(tree t)
+        {
+            double distance = DistanceTo(t.x, t.y);
+            if (distance == 0)
+            {
+                return true;
+            }
+            else if (distance < (radius * 2) + 5)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public double DistanceTo(double x1, double y1)
+        {
+            return Math.Sqrt(Math.Pow(x - x1, 2) + Math.Pow(y - y1, 2));
+        }
 
 
     }
